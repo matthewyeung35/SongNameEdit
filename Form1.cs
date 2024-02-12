56c05fc523c3229e48c5cfe7dbca7a92dc920530
Matthew Yeung <matthewyeung34@gmail.com>
2024-02-13 00:10:08 +0800

    .

--- Form1.cs
+++ Form1.cs
@@ -14,7 +14,25 @@ namespace SongNameEdit
 
         private void start_Click(object sender, EventArgs e)
         {
+            if (!Directory.Exists(folderPath.Text)) return;
             string[] fileList = Directory.GetFiles(folderPath.Text);
+            // Backup Folder
+            string backupFolderPath = Path.Combine(folderPath.Text, "backup");
+            Directory.CreateDirectory(backupFolderPath);
+            foreach (string file in fileList)
+            {
+                try
+                {
+                    string fileName = Path.GetFileName(file);
+                    string destinationFile = Path.Combine(backupFolderPath, fileName);
+                    File.Copy(file, destinationFile);
+                }
+                catch
+                {
+                }
+            }
+
+            // Rename
             foreach (string file in fileList)
             {
                 try {
@@ -28,29 +46,38 @@ namespace SongNameEdit
                     using (FlacFile flacFile = new FlacFile(file))
                     {
                         string songTitle = flacFile.VorbisComment.Title.ToString();
+                        string trackId = flacFile.VorbisComment.TrackNumber.ToString();
+                        if (!string.IsNullOrEmpty(trackId))
+                        {
+                            string validId = Regex.Replace(trackId, "[\\/:*?\"<>|]", "");
+                            newFileName += validId + " ";
+                            newFileName = new string(newFileName.Where(c => !Path.GetInvalidFileNameChars().Contains(c)).ToArray());
+                        }
+
                         if (!string.IsNullOrEmpty(songTitle))
                         {
                             string validSongTitle = Regex.Replace(songTitle, "[\\/:*?\"<>|]", "");
-                            newFileName = validSongTitle + ".flac";
+                            newFileName += validSongTitle;
                             newFileName = new string(newFileName.Where(c => !Path.GetInvalidFileNameChars().Contains(c)).ToArray());
-                            newFileName = Path.Combine(Path.GetDirectoryName(file), newFileName);
                         }
                     }
-                    File.Move(file, newFileName);
-                    //Remove trailing numbers from file name
-                    int i;
-                    for (i = 0; i < file.Length; i++)
-                    {
-                        if (!trailingNumbers.Contains(fileName[i]))
-                        {
-                            break;
-                        }
-                    }
-                    if (i > 0)
-                    {
-                        newFileName = Path.Combine(Path.GetDirectoryName(file), fileName.Substring(i));
-                        File.Move(file, newFileName);
-                    }
+                    newFileName = Path.Combine(Path.GetDirectoryName(file), newFileName);
+                    File.Move(file, newFileName + ".flac");
+
+                    ////Remove trailing numbers from file name
+                    //int i;
+                    //for (i = 0; i < file.Length; i++)
+                    //{
+                    //    if (!trailingNumbers.Contains(fileName[i]))
+                    //    {
+                    //        break;
+                    //    }
+                    //}
+                    //if (i > 0)
+                    //{
+                    //    newFileName = Path.Combine(Path.GetDirectoryName(file), fileName.Substring(i));
+                    //    File.Move(file, newFileName);
+                    //}
                 }
                 catch 
                 { 
